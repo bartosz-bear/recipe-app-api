fc381fb73bc09ffadcfebd6b58b0b68389968ec2
bartosz-bear <bartoszpiechnik@yahoo.com>
2025-01-19 16:26:36 +0100

    add psycopg2 to Dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add x and y and returns the result
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtract y from x and returns the result
+    """
+    return x - y
